7b7e74ec29b1134deb75200220ad3651f993b7c6
Subin Lim <sblim@i-screamedu.co.kr>
2023-05-08 13:15:08 +0900

    feat: 튜토리얼 버전 업데이트

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,18 @@
-module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
   },
 };
+export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
-import '../src/index.css';
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
 };
+
+export default preview;
