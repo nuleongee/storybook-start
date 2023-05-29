b7ae653e65a8f06c4403f6931ac43cf5b4bb7480
Subin Lim <sblim@i-screamedu.co.kr>
2023-05-30 00:03:29 +0900

    test: deprecated knobs 제거, Controls 대체

--- src/Hello/Hello.stories.js
+++ src/Hello/Hello.stories.js
@@ -1,21 +1,19 @@
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
-
 import Hello from './Hello';
 
 export default {
   title: 'components|basic/Hello', // 스토리북에서 보여질 그룹과 경로를 명시
   component: Hello, // 어떤 컴포넌트를 문서화 할지 명시
-  decorators: [withKnobs], // 애드온 적용
 };
 
-export const hello = () => {
-  // knobs 만들기
-  const big = boolean('big', false);
-  const name = text('name', 'Storybook');
-  return <Hello name={name} big={big} />;
+export const standard = {
+  args: {
+    big: false,
+    name: 'Storybook',
+  },
 };
-hello.story = {
-  name: 'Default',
+export const big = {
+  args: {
+    big: true,
+    name: 'Storybook',
+  },
 };
-export const standard = () => <Hello name="Storybook" />;
-export const big = () => <Hello name="Storybook" big />;
