a225ce380a935abad75ce1752bd58a4188695e0b
nuleongee <nuleongee@gmail.com>
2023-05-31 01:51:29 +0900

    test: actions 추가, controls 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,7 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
--- src/Hello/Hello.stories.js
+++ src/Hello/Hello.stories.js
@@ -3,17 +3,34 @@ import Hello from './Hello';
 export default {
   title: 'components|basic/Hello', // 스토리북에서 보여질 그룹과 경로를 명시
   component: Hello, // 어떤 컴포넌트를 문서화 할지 명시
+  argTypes: {
+    big: {
+      options: [true, false],
+      control: 'boolean',
+    },
+    name: { control: 'text' },
+    onHello: { action: 'onHello' },
+    onBye: { action: 'onBye' },
+  },
+  args: {
+    // big: true,
+    name: 'Storybook',
+  },
 };
 
 export const standard = {
   args: {
     big: false,
-    name: 'Storybook',
+    // name: 'Storybook',
+    // onHello: action('onHello'),
+    // onBye: action('onBye'),
   },
 };
 export const big = {
   args: {
     big: true,
-    name: 'Storybook',
+    // name: 'Storybook',
+    // onHello: action('onHello'),
+    // onBye: action('onBye'),
   },
 };
