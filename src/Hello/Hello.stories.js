6eb0685b82ca768bf2d189576349dab5379795c1
Subin Lim <sblim@i-screamedu.co.kr>
2023-06-01 23:54:43 +0900

    test: autodocs

--- src/Hello/Hello.stories.js
+++ src/Hello/Hello.stories.js
@@ -3,6 +3,7 @@ import Hello from './Hello';
 export default {
   title: 'components|basic/Hello', // 스토리북에서 보여질 그룹과 경로를 명시
   component: Hello, // 어떤 컴포넌트를 문서화 할지 명시
+  tags: ['autodocs'],
   argTypes: {
     big: {
       options: [true, false],
