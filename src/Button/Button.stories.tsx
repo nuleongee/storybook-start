544cebac86d80adb8fad4d0d79550df36da4cf10
Subin Lim <sblim@i-screamedu.co.kr>
2023-06-06 23:58:42 +0900

    feat: Button 설정 추가
    - theme, size, disabled, width

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+/** @jsx jsx */
 import Button from './Button';
+import { jsx, css } from '@emotion/react';
 
 export default {
   title: 'components/Button',
@@ -16,5 +17,76 @@ button.story = {
 };
 
 export const primaryButton = () => {
-  return <Button>PRIMARY</Button>;
+  return <Button theme="primary">PRIMARY</Button>;
+};
+
+export const secondaryButton = () => {
+  return <Button theme="secondary">SECONDARY</Button>;
+};
+
+export const tertiaryButton = () => {
+  return <Button theme="tertiary">TERTIARY</Button>;
+};
+
+const buttonWrapper = css`
+  .description {
+    margin-bottom: 0.5rem;
+  }
+  & > div + div {
+    margin-top: 2rem;
+  }
+`;
+
+export const size = () => {
+  return (
+    <div css={buttonWrapper}>
+      <div>
+        <div className="description">Samll</div>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div>
+        <div className="description">Medium</div>
+        <Button size="medium">BUTTON</Button>
+      </div>
+      <div>
+        <div className="description">Big</div>
+        <Button size="big">BUTTON</Button>
+      </div>
+    </div>
+  );
+};
+
+export const disabled = () => {
+  return (
+    <div css={buttonWrapper}>
+      <div>
+        <Button disabled theme="primary">
+          PRIMARY
+        </Button>
+      </div>
+      <div>
+        <Button disabled theme="secondary">
+          SECONDARY
+        </Button>
+      </div>
+      <div>
+        <Button disabled theme="tertiary">
+          TERTIARY
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export const customSized = () => {
+  return (
+    <div css={buttonWrapper}>
+      <div>
+        <Button width="20rem">CUSTOM WIDTH</Button>
+      </div>
+      <div>
+        <Button width="100%">FULL WIDTH</Button>
+      </div>
+    </div>
+  );
 };
